6e0cf9933b44109eebad64efbf4fe1e6a53876fb
penglei <1317821900@qq.com>
2018-08-08 11:23:00 +0800

    新增登录页面

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App'
 import config from '@/utils/config';
-
+import {cookieStorage} from '@/utils/index'
 var Fly = require("flyio/dist/npm/wx")
 var fly = new Fly;
 
+
 Vue.prototype.$http = fly;
 Vue.prototype.$config = config;
+Vue.prototype.$storage = cookieStorage;
+
 
 Vue.config.productionTip = false
 App.mpType = 'app'
@@ -25,19 +28,21 @@ AppData.textStatus = {
 }
 
 export default {
-  // 这个字段走 app.json
-  config: {
-    // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
-    pages: [
-        '^pages/index/main',
-        'pages/myActivity/main',
-        'pages/detail/main'
-    ],
-    window: {
-      backgroundTextStyle: 'light',
-      navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'WeChat',
-      navigationBarTextStyle: 'black'
+    // 这个字段走 app.json
+    config: {
+        // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
+        pages: [
+            '^pages/index/main',
+            'pages/myActivity/main',
+            'pages/detail/main',
+            'pages/register/main'
+        ],
+        window: {
+            backgroundTextStyle: 'light',
+            navigationBarBackgroundColor: '#fff',
+            navigationBarTitleText: 'WeChat',
+            navigationBarTextStyle: 'black',
+            backgroundColor: '#F3F3F3'
+        }
     }
-  }
 }
---
+++ src/pages/register/index.vue
@@ -0,0 +1,89 @@
+<template>
+    <div id="register">
+        <div class="phone__warning">
+            <image :src="logo"></image>
+            <!--<i class="iconfont icon-anquanjinggao"></i>-->
+        </div>
+        <button class="wechat-phoneBtn phone__btn" open-type="getPhoneNumber" @getphonenumber="getPhoneNumber">
+            <i class="iconfont icon-hudong"></i>
+            <div class="btn__text">微信登录</div>
+        </button>
+        <div class="again-phoneBtn phone__btn" @click="jumpLogin">
+            <i class="iconfont icon--dianhua"></i>
+            <div class="btn__text">手机号登录</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+
+            }
+        },
+        mounted(){
+
+        },
+        methods:{
+
+        }
+    }
+</script>
+
+<style rel="stylesheet/less" lang="less">
+    @import "../../../static/global.less";
+    #register{
+        //margin-top: 100px;
+        padding: 0 15px;
+        .phone__warning{
+            text-align:center;
+            width:80px;
+            height:80px;
+            margin:80px auto 180px auto;
+            border-radius:100%;
+            overflow:hidden;
+
+            image{
+                width: 100%;
+                height: 100%;
+            }
+            i{
+
+                color: #FFBE00;
+                font-size: 50px;
+            }
+        }
+        .phone__text{
+            font-size: 14px;
+            color: #888888;
+            line-height: 20px;
+            margin-bottom: 32px;
+        }
+        .phone__btn{
+            width: 100%;
+            padding:15px 0;
+            line-height: 1;
+            font-size: 17px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 3px;
+            &.wechat-phoneBtn{
+                color:#FFFFFF;
+                background: @globalColor;
+                border: @globalColor;
+                margin-bottom:20px;
+            }
+            &.again-phoneBtn{
+                color: @globalColor;
+                background: #ffffff;
+                border: 1px solid @globalColor;
+            }
+            i{
+                padding-right:10px;
+            }
+        }
+    }
+
+</style>
